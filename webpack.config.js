2d4117717cdb6935f3775aa7d93e1dfe0778b092
Thiago Paixão <tpaxadev@gmail.com>
2016-06-12 18:08:47 -0300

    done example

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+let path = require('path');
+let webpack = require('webpack');
+
+module.exports = {
+  entry: './assets/js/main.js',
+  output: { path: __dirname, filename: 'bundle.js' },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+};
